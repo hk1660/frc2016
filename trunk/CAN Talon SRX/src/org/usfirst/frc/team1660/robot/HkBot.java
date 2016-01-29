f43f3f03c00cbdebbaa19612cbc3a64b656d58e2
adonis user <a@b.com>
2016-01-28 20:19:18 -0500

    made objects for mechanical actuators. made basic methods that can be used in autonomous and teleop.

--- trunk/CAN Talon SRX/src/org/usfirst/frc/team1660/robot/HkBot.java
+++ trunk/CAN Talon SRX/src/org/usfirst/frc/team1660/robot/HkBot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1660.robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SampleRobot;
@@ -16,21 +17,35 @@ import org.usfirst.frc.team1660.robot.CamImage;
 public class HkBot extends SampleRobot {
 	
 	CamImage tinkoCam = new CamImage();
-	
+    Timer timerAuto = new Timer();
+    double timerA = timerAuto.get();
     RobotDrive tinkoDrive;
     Joystick xDrive = new Joystick(0);// uses/assigns ports 
     Joystick xMan = new Joystick(1);
     int session;
 	DigitalInput testLimit = new DigitalInput(0);
+	
+	
+	
 
 	// Channels for the wheels
-    CANTalon left1	= new CANTalon(1);
+    CANTalon left1	= new CANTalon(1); 
     CANTalon left2	= new CANTalon(2);
     CANTalon left3	= new CANTalon(3);
     CANTalon right1	= new CANTalon(4);
     CANTalon right2	= new CANTalon(5);
     CANTalon right3	= new CANTalon(6);
+    // Channels for manipulator
+    CANTalon spitLeft	= new CANTalon(7); 
+    CANTalon spitRight	= new CANTalon(8);
+    //actuator
+    CANTalon dart	= new CANTalon(9);
+    //pistons
+    Relay pusher = new Relay(1);
+    Relay compressor = new Relay(2);
     
+    Relay extra = new Relay(3);
+    CANTalon extra1 = new CANTalon(10);
     Encoder leftEnc = new Encoder(null, null);
     Encoder rightEnc = new Encoder(null, null);
     
@@ -49,7 +64,7 @@ public class HkBot extends SampleRobot {
     	
     	while(isAutonomous() && isEnabled()){
     		
-    		reachBreachScore();
+    		//reachBreachScore();
     		
 	
     	}
@@ -69,13 +84,13 @@ public class HkBot extends SampleRobot {
         	
         	
         	//Driving Method
-            right1.set(xDrive.getRawAxis(0));
+        /*  right1.set(xDrive.getRawAxis(0));
             right2.set(xDrive.getRawAxis(0)); 
             right3.set(xDrive.getRawAxis(0)); 
-            left1.set(xDrive.getRawAxis(-0));
-            left2.set(xDrive.getRawAxis(-0)); 
-            left3.set(xDrive.getRawAxis(-0)); 
-
+            left1.set(xDrive.getRawAxis(0*-1));
+            left2.set(xDrive.getRawAxis(0*-1)); 
+            left3.set(xDrive.getRawAxis(0*-1)); */
+            tinkoDrive.tankDrive(xDrive, 1, xDrive, 5);
             //Manipulator Methods
             Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
         }
@@ -87,45 +102,102 @@ public class HkBot extends SampleRobot {
     
    //AUTO MODE METHODS
 
-	//Method to reach teh D, Breach a Drivetrain D, & Score on Low Goal
+	//Method to reach the D, Breach a Drivetrain Def, & Score on Low Goal
 	private void reachBreachScore() {
 		
 		
-		//reach(based on time /LS)
+		//reach defense(based on time /LS)
+		if(timerA < 2){
+			goForward(1);
+			
+			
+		}
 		
-		tinkoDrive.arcadeDrive(moveValue, rotateValue, squaredInputs);
+		//tinkoDrive.arcadeDrive(moveValue, rotateValue, squaredInputs);
 		
-		//breach (based on time ?)
+		//breach rough terrain(based on time ?)
+		if(timerA < 3){
+			goForward(1);
+		}
 		
 		
 		//aim generally towards goal (based on gyro)
-		
-		
+			
 		//aim precisely at goal (based on camera)
 		
 		
-		//drive fwd to goal (based on time/ accel)
+		//drive fwd to goal (based on time)
 		
 		
 		//score boulder (based on time)
 		
 		
 		
-		//go back to "reach"
-		
-		
-		
-		
-		
+		//go back to "reach"		
+		   
 	}
-    
-    
-    
-    
-    
+	//go forward
+	public void goForward(double speed){
+		
+		left1.set(speed);
+		left2.set(speed);
+		left3.set(speed);
+		right1.set(-speed);
+		right2.set(-speed);
+		right3.set(-speed);
+	
+	}
+	// right or left
+	public void turnRight(double speed){
+		left1.set(0);
+		left2.set(0);
+		left3.set(0);
+		right1.set(speed);
+		right2.set(speed);
+		right3.set(speed);
+	
+	}
+	// right or left
+	public void turnLeft(double speed){
+		left1.set(speed);
+		left2.set(speed);
+		left3.set(speed);
+		right1.set(0);
+		right2.set(0);
+		right3.set(0);
+	}
+
+	//go backwards
+	public void goBackward(double speed){
+
+		left1.set(-speed);
+		left2.set(-speed);
+		left3.set(-speed);
+		right1.set(speed);
+		right2.set(speed);
+		right3.set(speed);
+
+
+
+	}
+	// shoots ball at given speed
+	public void shootBall(double speed){
+		manipLeft.set(speed);
+		manipRight.set(-speed);
+	}
+	//turns on act. motor at given speed
+	public void turnOnActuator(double speed){
+		dart.set(speed);
+	}
+	//move robot based on camera image
+	public void moveRobot(image image){
+		//determine how to move robot based on image
+		
+		//move robot
+	}
+	
+
+
+
 }
-
-
-
-
 
